f3c2a91d6240d39518acf05ebd9481f0a143f3a1
Amirhossein Heydarian <amirhossein261077@live.com>
2023-05-18 23:26:54 +0330

    small changes

--- agents/DeepQLearning.py
+++ agents/DeepQLearning.py
@@ -11,8 +11,8 @@ from utils.plot import plot_return
 class QNetwork(nn.Module):
     def __init__(self, state_size, action_size):
         super().__init__()
-        self.fc1 = nn.Linear(state_size, 128)
-        self.fc2 = nn.Linear(128, action_size)
+        self.fc1 = nn.Linear(state_size, 256)
+        self.fc2 = nn.Linear(256, action_size)
 
     def forward(self, state):
         x = F.relu(self.fc1(state))
