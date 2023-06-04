f4bc2700a5bc3ed02ad9e5ab3b631e566a4648c4
amirhosseinh77 <amirhossein261077@live.com>
2023-06-04 17:38:40 +0330

    compatible with nn models

--- agents/ActorCritic.py
+++ agents/ActorCritic.py
@@ -1,50 +1,20 @@
 import torch
-import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
 import numpy as np
+from models.nn import CategoricalPolicyNetwork, ValueNetwork
+from utils.buffer import ReplayBuffer
 from utils.plot import plot_return
-
-
-class Actor(nn.Module):
-    def __init__(self, state_dim, hidden_dim, action_dim):
-        super(Actor, self).__init__()
-        self.fc1 = nn.Linear(state_dim, hidden_dim)
-        self.fc2 = nn.Linear(hidden_dim, action_dim)
-    
-    def forward(self, state):
-        x = F.relu(self.fc1(state))
-        logits = self.fc2(x)
-        return F.softmax(logits, dim=-1)
-
-class Critic(nn.Module):
-    def __init__(self, state_dim, hidden_dim):
-        super(Critic, self).__init__()
-        self.fc1 = nn.Linear(state_dim, hidden_dim)
-        self.fc2 = nn.Linear(hidden_dim, 1)
-    
-    def forward(self, state):
-        x = F.relu(self.fc1(state))
-        value = self.fc2(x)
-        return value
 
 
 class ActorCriticAgent():
     def __init__(self, state_size, action_size, hidden_dim=128, lr=1e-3):
         self.device = "cuda" if torch.cuda.is_available() else "cpu"
         self.memory = []
-        self.actor =  Actor(state_size, hidden_dim, action_size).to(self.device)
-        self.critic = Critic(state_size, hidden_dim).to(self.device)
+        self.actor =  CategoricalPolicyNetwork(state_size, action_size, hidden_dim).to(self.device)
+        self.critic = ValueNetwork(state_size, hidden_dim).to(self.device)
         self.actor_optimizer = optim.Adam(self.actor.parameters(), lr=lr, weight_decay=1e-4)
         self.critic_optimizer = optim.Adam(self.critic.parameters(), lr=lr, weight_decay=1e-4)
-        self.max_gradient_norm = 0.5
-
-    def select_action(self, state):
-        state = torch.tensor(state).to(self.device)
-        probs = self.actor(state)
-        dist = torch.distributions.Categorical(probs)
-        action = dist.sample()
-        return action.item(), dist.log_prob(action)
 
     def push_memory(self, state, action_log_prob, reward, next_state, done):
         new_experience = [state, action_log_prob, reward, next_state, done]
@@ -62,21 +32,24 @@ class ActorCriticAgent():
         states_val = self.critic(state_tensor)
         next_states_val = self.critic(next_state_tensor)
 
-        advantages = rewards + gamma * next_states_val * torch.logical_not(dones) - states_val
-        actor_loss = (-action_log_probs * advantages.detach()).sum()
-        critic_loss = (advantages**2).mean()
+        # Compute Value Targets
+        value_targets = rewards + gamma * next_states_val * torch.logical_not(dones)
+        value_loss = F.mse_loss(states_val, value_targets)
 
-        self.actor_optimizer.zero_grad()
+        # Update Value network
         self.critic_optimizer.zero_grad()
+        value_loss.backward(retain_graph=True)
+        self.critic_optimizer.step()
+
+        # Compute Actor Loss
+        advantages = value_targets - states_val
+        actor_loss = -(action_log_probs * advantages.detach()).sum()
+
+        # Update Actor network
+        self.actor_optimizer.zero_grad()
         actor_loss.backward()
-        critic_loss.backward()
-
-        # gradients clipping
-        # torch.nn.utils.clip_grad_norm_(self.actor.parameters(), max_norm=self.max_gradient_norm)
-        # torch.nn.utils.clip_grad_norm_(self.critic.parameters(), max_norm=self.max_gradient_norm)
-
         self.actor_optimizer.step()
-        self.critic_optimizer.step()  
+
         self.memory = []
 
     def train(self, env, episodes):
@@ -86,8 +59,8 @@ class ActorCriticAgent():
             done = False
             state = env.reset()
             while not done:
-                action, action_log_prob = self.select_action(state)
-                next_state, reward, done, info = env.step(action)
+                action, action_log_prob = self.actor.select_action(state)
+                next_state, reward, done, info = env.step(action.item())
                 self.push_memory(state, action_log_prob, reward, next_state, done)
                 score += reward
                 state = next_state
